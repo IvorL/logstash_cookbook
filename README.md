7daa9e06f554fa548b9db4024327b4f5d5dd1a16
Ivor Lishman <tlishman@spartaglobal.com>
2018-10-18 16:20:13 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # logstash_cookbook
 
-TODO: Enter the cookbook description here.
+Logstash is a data processing pipeline capable of storing and manipulating data from multiple sources, such as AWS EC2 instances. It can be used in conjunction with FileBeat, MetricBeat, Elasticsearch and Kibana, which are open-source projects created by the Elastic company in order to monitor sytems.
 
+When Filebeat and MetricBeat are installed on a machine, they pass log and system metric data to Logstash.
+
+The recipe contained within this Cookbook provides the necessary provisioning steps for installing Logstash and its dependencies on a virtual machine. The unit and integration tests created using ChefSpec and InSpec test the provisioning recipe to ensure that the necessary repositories have been added, required packages have been installed, folders and files have been added, and that the packages are the correct version.
+
+In order to run the tests on the Cookbook, follow these steps:
+
+1. Clone the repository for this Cookbook.
+2. From the terminal (inside the cloned repository), run the following command to perform the unit tests:
+  ```
+  chef exec rspec spec
+  ```
+3. When the unit tests have passed, run the following command to perform the integration tests:
+```
+kitchen test
+```
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,14 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# Update the sources list
 apt_update("update") do
   action :update
 end
 
+# Install Java version 8 for logstash
 package("openjdk-8-jdk") do
   action :install
 end
 
+# Add a key for the elasticsearch repository
 bash("add_elasticsearch_key") do
   code "wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -"
   action :run
@@ -24,14 +28,17 @@ apt_repository("elastic5") do
   action :add
 end
 
+
 apt_update("update") do
   action :update
 end
 
+# Install logstash
 package("logstash") do
   action :install
 end
 
+# Add the elasticsearch 6 repository to uograde logstash
 apt_repository("elastic6") do
   uri "https://artifacts.elastic.co/packages/6.x/apt"
   distribution "stable"
@@ -39,42 +46,52 @@ apt_repository("elastic6") do
   action :add
 end
 
+# Logstash must be upgraded from version 5 to 6. Installing 6 directly from the repository doesn't work
 package("logstash") do
   action :upgrade
 end
 
+# Delete existing startup.options file on the VM
 file("/usr/share/logstash/bin/startup.options") do
   action :delete
 end
 
+# Add a new startup.options file
 template("/usr/share/logstash/bin/startup.options") do
   source "startup.options.erb"
 end
 
+# Delete existing jvm.options file on the VM
 file("/etc/logstash/jvm.options") do
   action :delete
 end
 
+# Add a new jvm.options file
 template("/etc/logstash/jvm.options") do
   source "jvm.options.erb"
 end
 
+# Delete existing logstash.yml file on the VM
 file("/etc/logstash/logstash.yml") do
   action :delete
 end
 
+# Add a new logstash.yml file
 template("/etc/logstash/logstash.yml") do
   source "logstash.yml.erb"
 end
 
+# Create a conf.d directory on the VM for the logstash.conf file
 directory "/etc/logstash/conf.d" do
   action :create
 end
 
+# Add a logstash.conf file
 template("/etc/logstash/conf.d/logstash.conf") do
   source "logstash.conf.erb"
 end
 
+# Enable and start the logstash service
 service("logstash") do
   action [:enable, :start]
 end
